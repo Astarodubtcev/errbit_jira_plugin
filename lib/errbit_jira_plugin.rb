cbcbcf8e6522eb9a6edc19945dbfc9906880043c
Alexander Starodubtcev <astarodubtcev@yandex.ru>
2015-08-06 13:19:30 +0300

    reverted to v 0.3

--- lib/errbit_jira_plugin.rb
+++ lib/errbit_jira_plugin.rb
@@ -1,3 +1,4 @@
+
 require 'errbit_jira_plugin/version'
 require 'errbit_jira_plugin/error'
 require 'errbit_jira_plugin/issue_tracker'
@@ -6,10 +7,6 @@ require 'errbit_jira_plugin/rails'
 module ErrbitJiraPlugin
   def self.root
     File.expand_path '../..', __FILE__
-  end
-
-  def self.read_static_file(file)
-    File.read(File.join(self.root, 'vendor/assets/images', file))
   end
 end
 
